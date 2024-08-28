ffef777b2d886790835c40c47f9b904cf269905c
zhiyi tang <zhiyit@unc.edu>
2024-08-28 08:53:20 -0400

    Functions

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,24 @@
+"""CQ00 challenge question in class"""
+
+__author__ = "730736518"
+
+
+def mimic(message: str) -> str:
+    return message
+
+
+"""function takes input and repeat back to me"""
+
+print(mimic(message="three"))
+
+
+def main() -> None:
+    return None
+
+
+"""main function that pulls together my functions"""
+print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
---
+++ exercises/Functions.py
@@ -0,0 +1,7 @@
+def sum(num1: int, num2: int) -> float:
+    return num1 + num2
+
+
+a = sum(num1=1, num2=2)
+
+print(a)
